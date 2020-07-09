8f557c5e7f4a90df87f10ba3d4a6cbbb9d913e24
kobato16 <kobatok162@gmail.com>
2020-07-09 11:52:26 -0700

    greeting by user name also I guess

--- t.py
+++ t.py
@@ -2,5 +2,6 @@
 def printHello(name):
   print(f"Hello, {name}")
  
-printHello(input("Enter your nickname: "))
-print("Hello again")
+printHello(input("Enter your name: "))
+printHello(input("Enter you name: "))
+print("Hello again mudafukka")
